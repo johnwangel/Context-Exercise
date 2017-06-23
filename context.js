cf14317ac1455a58dfda2c38ca974130b40715fb
John Atkins <johnatkins1999@yahoo.com>
2017-06-22 15:16:11 -1000

    zsh:1: command not found: qw

--- context.js
+++ context.js
@@ -17,14 +17,17 @@ var wall_e = new Robot("Wall-E");
 
 
 // #1
-console.log(  );
+console.log(add( 0, 1 ));
 
-// #2  
-console.log(  );
+// #2
+console.log(add.call(voltron, 2, 3));
 
 // #3
-console.log(  );
+console.log(add.apply(optimus, [20, 30]));
 
 // #4
+var calculate = add.bind(bender);
+console.log(calculate("drinking ", "beer."));
 
-console.log(  );
+var dinner = add.bind(wall_e);
+console.log(dinner(23, 55));
